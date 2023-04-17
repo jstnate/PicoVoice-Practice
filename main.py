b5b15da0bfd2b4988f355548b1da1a82e2a13143
Nathan Lefetey <lefetey.nathan@gmail.com>
2023-04-17 21:21:26 +0200

    :lipstick: Add UI
    Add interface that display input and output

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import pvporcupine
 import pyaudio
 import struct
 from pocketsphinx import LiveSpeech, get_model_path
+import tkinter as tk
 
 ACCESS_KEY = "qrMNMOzh6k1ZdI6MMJCk1/U6NsSV2RsFBaFkLmX9al3pF+DMRnwoZA=="
 
-def detect_wake_word():
+def detect_word():
     porcupine = None
     pa = None
     audio_stream = None
@@ -26,6 +27,7 @@ def detect_wake_word():
         )
 
         print("Dites 'picovoice' pour commencer la traduction...")
+        infos.config(text="Dites 'picovoice' pour commencer la traduction..")
 
         while True:
             pcm = audio_stream.read(porcupine.frame_length)
@@ -34,6 +36,7 @@ def detect_wake_word():
 
             if keyword_index >= 0:
                 print("Mot-clé détecté, veuillez parler en anglais...")
+                infos.config(text="Mot-clé détecté, veuillez parler en anglais...")
                 return
 
     finally:
@@ -51,17 +54,21 @@ def recognize_speech():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         print("Enregistrement de votre voix...")
+        infos.config(text="Enregistrement de votre voix...")
         audio = recognizer.listen(source, timeout=3, phrase_time_limit=5)
 
     try:
         print("Reconnaissance vocale en cours...")
+        infos.config(text="Reconnaissance vocale en cours...")
         text = recognizer.recognize_sphinx(audio)
         return text
     except sr.UnknownValueError:
-        print("Désolé, je n'ai pas pu comprendre votre voix.")
+        print("Désolé, l'audio n'a pas pu être traduit.")
+        infos.config(text="Désolé, l'audio n'a pas pu être traduit.")
         return None
-    except sr.RequestError as e:
-        print(f"Une erreur s'est produite lors de la demande de reconnaissance vocale : {e}")
+    except sr.RequestError as err:
+        print(f"Une erreur s'est produite lors de la demande de reconnaissance vocale : {err}")
+        infos.config(text=f"Une erreur s'est produite lors de la demande de reconnaissance vocale : {err}")
         return None
 
 
@@ -72,14 +79,26 @@ def translate_text(text):
 
 
 def main():
-    while True:
-        detect_wake_word()
-        text = recognize_speech()
-        if text is not None:
-            print(f"Texte en anglais : {text}")
-            translated_text = translate_text(text)
-            print(f"Texte traduit en français : {translated_text}")
+    detect_word()
+    text = recognize_speech()
+    if text is not None:
+        print(f"Texte en anglais : {text}")
+        input.config(text=f"Texte en anglais : {text}")
+        translated_text = translate_text(text)
+        print(f"Texte traduit en français : {translated_text}")
+        output.config(text=f"Texte traduit en français : {translated_text}")
+        infos.config(text="Votre texte à été traduit avec succès !")
 
+window = tk.Tk()
+window.title("Traducteur vocal")
+window.geometry("500x500")
 
-if __name__ == "__main__":
-    main()
+button = tk.Button(window, text="Enregistrer", font=("Arial", 20), command=main)
+button.pack()
+infos = tk.Label(window, text="Cliquez sur le bouton pour commencer la traduction", font=("Arial", 20))
+infos.pack()
+input = tk.Label(window, text="Votre texte sera afficher ici", font=("Arial", 20))
+input.pack()
+output = tk.Label(window, text="Votre texte traduit sera afficher ici", font=("Arial", 20))
+output.pack()
+window.mainloop()
